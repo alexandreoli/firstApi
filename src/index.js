42c7313f4c7a71c7c48540db081b4c7bd840e08c
alexandreoli <57917511+alexandreoli@users.noreply.github.com>
2023-03-01 09:24:27 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const { URL } = require("url");
+
+const routes = require("./routes");
+const bodyParser = require("./helpers/bodyParser");
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(`request method: ${request.method} | EndPoint: ${request.url}`);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+  const splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
+  console.log(splitEndpoint);
+
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === pathname && routeObj.method === request.method
+  );
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { "Content-type": "application/json" });
+      response.end(JSON.stringify(body));
+    };
+
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { "Content-type": "text/html" });
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+});
+
+server.listen(3000, () =>
+  console.log("👾 👾 👾 server started at http://localhost:3000 👾 👾 👾")
+);
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'alexandre',
+  },
+  {
+    id: 2,
+    name: 'luiz',
+  },
+  {
+    id: 3,
+    name: 'gab',
+  },
+];
